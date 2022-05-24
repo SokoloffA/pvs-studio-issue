4f4b7222c952545a7132c6e8811439adc265ae0b
SokoloffA <sokoloff.a@gmail.com>
2022-05-24 20:39:07 +0300

    Check if the suppressions of other diagnostic rules work (fixed).

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@ mkdir -p build && cd build
 
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=On .. && make
 
-pvs-studio-analyzer analyze -R rules.pvsconfig -o PVS.log
+pvs-studio-analyzer analyze -R ../pvs-studio.pvsconfig -o PVS.log
 
 plog-converter -t tasklist -o PVS.tasks PVS.log
